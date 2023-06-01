2b00bcf4d6870826aca4d67d0d5f19d8898b00b5
moaliyou <moaliyou47@gmail.com>
2023-06-01 16:14:23 +0300

    Added menu function for delete all

--- app/src/main/java/com/example/book_library_app/MainActivity.java
+++ app/src/main/java/com/example/book_library_app/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.book_library_app;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -74,5 +78,20 @@ public class MainActivity extends AppCompatActivity {
                         new Intent(MainActivity.this, AddBookActivity.class)
                 )
         );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.delete_menu) {
+            Toast.makeText(this, "Delete All", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
